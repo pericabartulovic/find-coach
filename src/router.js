62299a72f60f0c8344c4460d7f5e61ba9837ee9b
pericabartulovic <pericab35@gmail.com>
2022-07-08 12:25:04 +0200

    adding vuex, lists, base cmps

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ const router = createRouter({
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
-      path: '/coaches/id',
+      path: '/coaches/:id',
       component: CoachDetail,
       children: [
         { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,34 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters';
+
+export default {
+  namespaced: true,      //to make sure that multiple modules are clearly separated from each other (and to avoid name clashes)
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Maximilian',
+          lastName: 'Schwarzmüller',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30
+        },
+        {
+          id: 'c2',
+          firstName: 'Julie',
+          lastName: 'Jones',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30
+        }
+      ]
+    }
+  },
+  mutations,  // umjesto mutations: mutations
+  actions,
+  getters
+}
