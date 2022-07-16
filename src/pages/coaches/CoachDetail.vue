ded056c4b4050086d749a81e9713af9d84c98025
pericabartulovic <pericab35@gmail.comc>
2022-07-16 23:41:49 +0200

    CoachDetail.vue removed button

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -10,7 +10,7 @@
       <base-card>
         <header>
           <h2>Interested? Reach out now!</h2>
-          <base-button link :to="contactLink">Contact</base-button>
+          <!-- <base-button link :to="contactLink">Contact</base-button> ovaj gumb tu nema nikakvu funkciju  -->
         </header>
         <router-view></router-view>
       </base-card>
@@ -51,7 +51,7 @@ export default {
       return this.selectedCoach.description;
     },
     contactLink() {
-      return this.$route.path + '/contact';
+      return this.$route.path + '/' + this.id + '/contact';
     },
   },
   created() {
