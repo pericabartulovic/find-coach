39d5cc58e650a1ee5d7edb1f476ed69c425cee1c
pericabartulovic <pericab35@gmail.com>
2022-07-18 13:28:44 +0200

    user authentication

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,53 +1,95 @@
+let timer;
+
 export default {
   async login(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCU41xv7Kh7Ehxr_JyzzOpUFfXn4rL2YuE', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true
-      }),
-    });
-    const resposeData = await response.json();
-    if(!response.ok) {
-      console.log(resposeData);
-      const errror = new Error(resposeData.message || 'Failed to authenticate. Check your login data.');
-      throw errror;
-    }
-    context.commit('setUser', {
-      token : resposeData.idToken,
-      userId: resposeData.localId,
-      tokenExpiration: resposeData.expiresIn
+    return context.dispatch('auth', {  //obavezno return da vrati promise
+      ...payload,
+      mode: 'login'
     });
   },
   async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCU41xv7Kh7Ehxr_JyzzOpUFfXn4rL2YuE', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true
-      }),
+    return context.dispatch('auth', {  //obavezno return da vrati promise
+      ...payload,
+      mode: 'signup'
     });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCU41xv7Kh7Ehxr_JyzzOpUFfXn4rL2YuE';
     
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCU41xv7Kh7Ehxr_JyzzOpUFfXn4rL2YuE'
+    }
+    
+    const response = await fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        }),
+      });
+      
     const resposeData = await response.json();
     if(!response.ok) {
-      console.log(resposeData);
       const errror = new Error(resposeData.message || 'Failed to authenticate. Check your login data.');
       throw errror;
     }
+    
+    const expiresIn = +resposeData.expiresIn * 1000;
+    // const expiresIn = 5000;  // 5 sekundi u svrhu testiranja samo
+    const expirationDate = new Date().getTime() + expiresIn;
+    
+    localStorage.setItem('token', resposeData.idToken);
+    localStorage.setItem('userId', resposeData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+    
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn)
+    
     context.commit('setUser', {
       token : resposeData.idToken,
       userId: resposeData.localId,
-      tokenExpiration: resposeData.expiresIn
     });
   },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+    
+    const expiresIn = +tokenExpiration - new Date().getTime();   //razlika u milisekundama između budućeg vremena kad token istječe i trenutnog vremena.
+    
+    if (expiresIn < 0) {
+      return;
+    }
+    
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+    
+    if(token && userId) {
+      context.commit('setUser', {
+        token,
+        userId
+      });
+    }
+  },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+    
+    clearTimeout(timer);
+    
     context.commit('setUser', {
       userId: null,
       token: null,
-      tokenExpiration: null
     })
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   }
-  
 };
--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -10,7 +10,7 @@ export default {
     return {
       userId: null,
       token: null,
-      tokenExpiration: null
+      didAutoLogout: false
     };
   },
   
