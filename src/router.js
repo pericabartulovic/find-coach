1646aec11eb9e6a0b6ea84e3b197972244993f80
pericabartulovic <pericab35@gmail.comc>
2022-07-19 23:59:39 +0200

    app deploy

--- src/router.js
+++ src/router.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import UserAuth from './pages/auth/UserAuth.vue'
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 import store from './store/index.js'
+
+
+// Uvoz komponenti po potrebi (da se ne skidaju ako ih user ne treba) kao async defying ali se kod routera na prepporuča
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,7 +1,6 @@
 export default {
   addRequest(state, payload) {
     state.requests.push(payload);
-    console.log(state.requests)
   },
   setRequests(state, payload) {
     state.requests = payload;
